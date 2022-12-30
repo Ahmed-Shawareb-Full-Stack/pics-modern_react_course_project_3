ae4de9ed80b1ce965292266e1dd2483e8d74fd05
Ahmed Shawareb <js.dev.ahmedshawareb@gmail.com>
2022-12-30 23:35:30 +0200

    add functionality to SearchBar

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,12 +1,28 @@
 //! ─── Imports ─────────────────────────────────────────────────────────────────
 
 import React from "react";
+import { useState } from "react";
 import "./style.scss";
 
 //! ─── React Component ─────────────────────────────────────────────────────────
 
-function SearchBar() {
-  return <div>SearchBar</div>;
+function SearchBar({ onSubmit }) {
+  const [term, setTerm] = useState("");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmit(term);
+  };
+
+  const handleChange = (event) => {
+    setTerm(event.target.value);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input value={term} onChange={handleChange} />
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
