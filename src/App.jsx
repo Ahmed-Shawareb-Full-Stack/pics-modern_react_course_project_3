13f442b601c1aef9bc81f9858b12d39fe21e01b2
Ahmed Shawareb <js.dev.ahmedshawareb@gmail.com>
2022-12-30 23:36:42 +0200

    use state lift up to share data between siblings components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+//! ─── Imports ─────────────────────────────────────────────────────────────────
+
+import { useState } from "react";
+
 import "./App.scss";
+import SearchBar from "./components/SearchBar";
+import searchImage from "./api";
+import ImageList from "./components/ImageList/index";
+
+//! ─── React App ───────────────────────────────────────────────────────────────
 
 function App() {
-  return <div className="App">PICS</div>;
+  const [images, setImages] = useState([]);
+
+  const handleSubmit = async (term) => {
+    const results = await searchImage(term);
+
+    setImage(results);
+  };
+  return (
+    <div className="App">
+      <SearchBar onSubmit={handleSubmit} />
+      <ImageList images={images} />
+    </div>
+  );
 }
 
 export default App;
